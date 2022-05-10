0c6ae9e4336168249b1c6b9023f553b28e3cf08d
Gurveen <gurveensingh45@gmail.com>
2022-05-10 13:19:20 +0530

    Int

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Calendar from "./components/calendar/calendar";
+
+const style = {
+  position: "relative",
+  margin: "50px auto",
+};
+
+class App extends Component {
+  onDayClick = (e, day) => {
+    console.log("day: ", day);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Calendar
+          style={style}
+          width="302px"
+          onDayClick={(e, day) => this.onDayClick(e, day)}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
